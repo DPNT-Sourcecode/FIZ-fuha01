7df88b19b5f22c8d23314c1ee7621223876aa45b
sbx_user1060 <sbx_user1060@ip-10-128-34-238.eu-west-2.compute.internal>
2018-08-21 20:43:02 +0000

    Tue Aug 21 20:43:02 UTC 2018

--- src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
+++ src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
@@ -4,7 +4,7 @@ import befaster.runner.SolutionNotImplementedException;
 
 public class FizzBuzzSolution {
 
-    public String fizzBuzz(Integer number) {
+    public String fizzBuzz1(Integer number) {
     	
     	StringBuilder sb = new StringBuilder();
     	
@@ -22,6 +22,50 @@ public class FizzBuzzSolution {
     	}
     	return sb.toString();
         
+    }
+    
+public String fizzBuzz(Integer number) {
+    	
+    	StringBuilder sb = new StringBuilder();
+    	
+    	if(isFizz(number))
+    	{
+    		sb.append("fizz");
+    	}
+    	if(isBuzz(number))
+    	{    	    
+    		sb= (sb.length()==0)?sb.append("buzz"):sb.append(" buzz");    		
+    	}
+    	if(sb.length()==0)
+    	{
+    		sb.append(number.toString());
+    	}
+    	return sb.toString();
+        
+    }
+    
+    private boolean isFizz(Integer number){
+    	
+    	if(number%3==0||number.toString().indexOf("3")!=-1)
+    	{
+		   return true;
+    	}
+		else{
+	
+		   return false;
+    	}
+    }
+    
+    private boolean isBuzz(Integer number){
+    	if(number%5==0||number.toString().indexOf("5")!=-1)
+    	{
+		   return true;
+    	}
+		else{
+	
+		   return false;
+    	}
+    	
     }
 
 }
