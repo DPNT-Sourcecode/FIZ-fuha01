08340db43974ef30c1a6d4d265ebff20b1804caf
sbx_user1060 <sbx_user1060@ip-10-128-34-238.eu-west-2.compute.internal>
2018-08-21 21:10:14 +0000

    Tue Aug 21 21:10:14 UTC 2018

---
+++ src/main/java/befaster/solutions/FIZ/FizzBuzz.java
@@ -0,0 +1,13 @@
+package befaster.solutions.FIZ;
+
+public interface FizzBuzz {
+
+	public boolean isFizz(Integer number);
+	
+	public boolean isBuzz(Integer number);
+	
+	public boolean isDeluxe(Integer number);
+	
+	public boolean isFakeDeluxe(Integer number);
+	
+}
