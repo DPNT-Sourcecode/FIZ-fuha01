de44f510755612f1db1783ed22611fdde382b77b
sbx_user1060 <sbx_user1060@ip-10-128-34-238.eu-west-2.compute.internal>
2018-08-21 21:23:26 +0000

    Tue Aug 21 21:23:26 UTC 2018

--- src/main/java/befaster/solutions/FIZ/FizzBuzzImplementation.java
+++ src/main/java/befaster/solutions/FIZ/FizzBuzzImplementation.java
@@ -33,8 +33,8 @@ public class FizzBuzzImplementation implements FizzBuzz {
 
 	@Override
 	public boolean isDeluxe(Integer number) {
-		if((number%3==0&&number.toString().indexOf("3")!=-1)
-    			||(number%5==0&&number.toString().indexOf("5")!=-1)
+		if(((number%3==0&&number.toString().indexOf("3")!=-1)
+    			||(number%5==0&&number.toString().indexOf("5")!=-1))
     			&& isEven(number)){
     		return true;
     	}else{
